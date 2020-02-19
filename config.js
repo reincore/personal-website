39c2fb0bf2889bffe24772518c259a4de9f385b4
Deniz Saglam <deniz.saglam@accenture.com>
2020-02-19 15:41:50 +0300

    Edited main page texts, changed photo.jpg

--- config.js
+++ config.js
@@ -1,49 +1,51 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Blog by Deniz Sağlam",
+  subtitle:
+    "Front-end web developer from Istanbul, Turkey. Current tech focus: ReactJS, ES6+, Sass",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/"
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about"
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: "Contact me",
+      path: "/pages/contacts"
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "Deniz Sağlam",
+    photo: "/photo.jpg",
+    bio:
+      "Front-end web developer from Istanbul, Turkey. Current tech focus: ReactJS, ES6+, Sass",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
+      email: "dnzsaglam@gmail.com",
+      facebook: "dnzsaglam",
+      telegram: "#",
+      twitter: "saglamspor",
+      github: "reincore",
+      rss: "",
+      vkontakte: "",
+      linkedin: "saglamdeniz",
+      instagram: "denizsaglam",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: ""
     }
   }
 };
